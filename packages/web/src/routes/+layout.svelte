2868a0c95fc1c77a196a43ac924f5bb73c4a8b67
good-jinu <gooday6983@gmail.com>
2025-10-20 02:27:07 +0900

    Update favicon.ico

--- packages/web/src/routes/+layout.svelte
+++ packages/web/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 import "../app.css";
-import favicon from "$lib/assets/favicon.svg";
+import favicon from "$lib/assets/favicon.ico";
 import ThemeProvider from "$lib/components/ThemeProvider.svelte";
 import ThemeToggle from "$lib/components/ThemeToggle.svelte";
 import { Toaster } from "$lib/components/ui/sonner";
